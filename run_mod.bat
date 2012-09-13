7652add6661ad402323d19b098fac5d5fd9b0458
Riley Labrecque <rileylabrecque@gmail.com>
2012-09-13 14:45:42 -0600

    Initial import from the SDK

---
+++ run_mod.bat
@@ -0,0 +1,1 @@
+@"c:\games\steam\steamapps\dafox1\source sdk base 2007\hl2.exe" -dev -game "c:\games\steam\steamapps\SourceMods\source2007hl2mp" -allowdebug %1 %2 %3 %4 %5 %6 %7 %8 %9
---
+++ run_studiomdl.bat
@@ -0,0 +1,2 @@
+@"c:\games\steam\steamapps\dafox1\sourcesdk\bin\source2007\bin\studiomdl.exe" -game "c:\games\steam\steamapps\SourceMods\source2007hl2mp" %1 %2 %3 %4 %5 %6 %7 %8 %9
+
